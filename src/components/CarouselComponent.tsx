3f9550b410bf270a3ef785042bec8c2cef85af4d
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-05-25 15:54:20 +0200

    swiper carousel

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -1,5 +1,53 @@
+import "swiper/swiper-bundle.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import cover1 from "/imgs/cover1.webp";
+import cover2 from "/imgs/cover2.webp";
+import cover3 from "/imgs/cover3.webp";
+import cover4 from "/imgs/cover4.webp";
+import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
 const CarouselComponent = () => {
-    return <div>CarouselComponent</div>;
+    const [arrayImgs, setArrayImgs] = useState<null | string[]>(null);
+
+    useEffect(() => {
+        setArrayImgs([cover1, cover2, cover3, cover4]);
+    }, []);
+
+    return (
+        <>
+            <Container fluid>
+                <Row>
+                    <Col className="p-0">
+                        <div>
+                            <Swiper
+                                slidesPerView={1}
+                                navigation
+                                pagination={{ clickable: true }}
+                                scrollbar={{ draggable: true }}
+                                draggable="true"
+                                loop={true}
+                                autoplay={true}
+                            >
+                                {arrayImgs &&
+                                    arrayImgs.map((img, i) => (
+                                        <SwiperSlide key={`ciao-${i}`}>
+                                            <div>
+                                                <img
+                                                    style={{ width: "100%", height: "500px", objectFit: "cover" }}
+                                                    src={img}
+                                                    alt="esercizio"
+                                                />
+                                            </div>
+                                        </SwiperSlide>
+                                    ))}
+                            </Swiper>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 };
 
 export default CarouselComponent;
---
+++ src/functions/localHostPath.ts
@@ -0,0 +1,4 @@
+// stringa url localhost per fetch
+
+// connessione al local host per fetch
+export const LocalHostPath: string = "https://localhost:7098";
