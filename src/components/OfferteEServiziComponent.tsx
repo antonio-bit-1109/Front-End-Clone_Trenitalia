a0bffeedaee216cfad5bf9d5da53331987818f01
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-05-27 18:42:08 +0200

    fix swiper

--- src/components/OfferteEServiziComponent.tsx
+++ src/components/OfferteEServiziComponent.tsx
@@ -1,84 +1,145 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getImages } from "../fetches/GET/getFetches";
 import { AppDispatch } from "../redux/store/store";
 import { rootState } from "../interfaces/Interfaces";
 import { LocalHostPath } from "../functions/localHostPath";
+import "swiper/swiper-bundle.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, Autoplay } from "swiper/modules";
 
 const OfferteEServiziComponent = () => {
     const dispatch: AppDispatch = useDispatch();
 
     const { imagesObjs } = useSelector((store: rootState) => store.main);
+    const { widthWindow } = useSelector((store: rootState) => store.main);
+
+    const [cardPerWiew, setCardPerView] = useState<"auto" | number | undefined>(1);
 
     useEffect(() => {
         dispatch(getImages());
     }, [dispatch]);
 
+    useEffect(() => {
+        changeSwiperCardVisibility(widthWindow);
+    }, [widthWindow]);
+
+    const changeSwiperCardVisibility = (widthWindow: number) => {
+        if (widthWindow < 780) {
+            setCardPerView(1);
+            return;
+        }
+        if (widthWindow < 1200) {
+            setCardPerView(2);
+            return;
+        }
+        setCardPerView(3);
+        return;
+    };
+
     return (
         <>
             <Container>
-                <div className="text-center d-flex flex-column align-items-center">
+                <Row>
+                    <Col>
+                        <div className="my-3 fs-4 fw-bold">OFFERTE E SERVIZI</div>
+                    </Col>
+                </Row>
+                <div className="mt-2">
                     <Row>
                         <Col>
-                            <div className="my-3 fs-4 fw-bold">OFFERTE E SERVIZI</div>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <div className="d-flex my-3">
+                            <Swiper
+                                slidesPerView={cardPerWiew}
+                                navigation={true}
+                                pagination={{ clickable: true }}
+                                scrollbar={{ draggable: true }}
+                                loop={true}
+                                // autoplay={{ delay: 6000 }}
+                                modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+                            >
                                 {imagesObjs &&
                                     imagesObjs.slice(0, 3).map((obj, i) => (
-                                        <div className="position-relative mx-2" key={`my-image-key-${i}`}>
-                                            <img
-                                                className="rounded-4"
-                                                src={`${LocalHostPath}/imgs/${obj.image}`}
-                                                alt="immagine"
-                                            />
-                                            <p className="position-absolute bottom-0 fw-bolder text-light text-center primo-testo">
-                                                {obj.title.toUpperCase()}
-                                            </p>
-                                        </div>
+                                        <SwiperSlide key={`my-image-key-${i}`}>
+                                            <div className="d-flex justify-content-center mt-2 ">
+                                                {" "}
+                                                <img
+                                                    className="rounded-4 mx-2 position-relative"
+                                                    src={`${LocalHostPath}/imgs/${obj.image}`}
+                                                    alt="immagine"
+                                                />
+                                                <div className="positioning text-center">
+                                                    <p className="fw-bolder">{obj.title.toUpperCase()}</p>
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
                                     ))}
-                            </div>
+                            </Swiper>
                         </Col>
                     </Row>
+                </div>
+                <div className="mt-2">
                     <Row>
                         <Col>
-                            <div className="d-flex mb-3">
+                            <Swiper
+                                slidesPerView={cardPerWiew}
+                                navigation={true}
+                                pagination={{ clickable: true }}
+                                scrollbar={{ draggable: true }}
+                                loop={true}
+                                // autoplay={{ delay: 6000 }}
+                                modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+                            >
                                 {imagesObjs &&
                                     imagesObjs.slice(3, 6).map((obj, i) => (
-                                        <div className="position-relative mx-2" key={`my-image-key-${i}`}>
-                                            <img
-                                                className="rounded-4"
-                                                src={`${LocalHostPath}/imgs/${obj.image}`}
-                                                alt="immagine"
-                                            />
-                                            <p className="position-absolute bottom-0 fw-bolder text-light text-center primo-testo">
-                                                {obj.title.toUpperCase()}
-                                            </p>
-                                        </div>
+                                        <SwiperSlide key={`my-image-key-${i}`}>
+                                            <div className="d-flex justify-content-center mt-2 ">
+                                                {" "}
+                                                <img
+                                                    className="rounded-4 mx-2 position-relative"
+                                                    src={`${LocalHostPath}/imgs/${obj.image}`}
+                                                    alt="immagine"
+                                                />
+                                                <div className="positioning text-center">
+                                                    <p className="fw-bolder">{obj.title.toUpperCase()}</p>
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
                                     ))}
-                            </div>
+                            </Swiper>
                         </Col>
                     </Row>
+                </div>
+                <div className="mt-2">
                     <Row>
                         <Col>
-                            <div className="d-flex">
+                            <Swiper
+                                slidesPerView={cardPerWiew}
+                                slidesPerGroup={1}
+                                navigation={true}
+                                pagination={{ clickable: true }}
+                                scrollbar={{ draggable: true }}
+                                loop={true}
+                                // autoplay={{ delay: 6000 }}
+                                modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+                            >
                                 {imagesObjs &&
-                                    imagesObjs.slice(6).map((obj, i) => (
-                                        <div className="position-relative mx-2" key={`my-image-key-${i}`}>
-                                            <img
-                                                className="rounded-4"
-                                                src={`${LocalHostPath}/imgs/${obj.image}`}
-                                                alt="immagine"
-                                            />
-                                            <p className="position-absolute bottom-0 fw-bolder text-light primo-testo">
-                                                {obj.title.toUpperCase()}
-                                            </p>
-                                        </div>
+                                    imagesObjs.slice(6, 9).map((obj, i) => (
+                                        <SwiperSlide key={`my-image-key-${i}`}>
+                                            <div className="d-flex justify-content-center mt-2 ">
+                                                {" "}
+                                                <img
+                                                    className="rounded-4 mx-2 position-relative"
+                                                    src={`${LocalHostPath}/imgs/${obj.image}`}
+                                                    alt="immagine"
+                                                />
+                                                <div className="positioning text-center">
+                                                    <p className="fw-bolder">{obj.title.toUpperCase()}</p>
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
                                     ))}
-                            </div>
+                            </Swiper>
                         </Col>
                     </Row>
                 </div>
