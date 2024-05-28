f82f1de58c2b31886af5cf197ff77992716b74f5
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-05-28 14:37:47 +0200

    update jumbotron

--- src/components/JumbotronMainCompo.tsx
+++ src/components/JumbotronMainCompo.tsx
@@ -1,5 +1,31 @@
+import { Col, Container, Row } from "react-bootstrap";
+import longImg from "/imgs/jumbotronLong.webp";
+import ShortImg from "/imgs/jumbotronShort.webp";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { rootState } from "../interfaces/Interfaces";
+
 const JumbotronMainCompo = () => {
-    return <></>;
+    const { widthWindow } = useSelector((store: rootState) => store.main);
+    const [img, setImg] = useState<undefined | string>("");
+
+    useEffect(() => {
+        widthWindow < 1100 ? setImg(ShortImg) : setImg(longImg);
+    }, [widthWindow]);
+
+    return (
+        <div className="mb-mdd-3">
+            <Container fluid className="p-0">
+                <Row>
+                    <Col>
+                        <div>
+                            <img className="w-100" src={img} alt="" />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 };
 
 export default JumbotronMainCompo;
---
+++ src/components/LavoriProgrammatiComponent.tsx
@@ -0,0 +1,5 @@
+const LavoriProgrammatiComponent = () => {
+    return <div>LavoriProgrammatiComponent</div>;
+};
+
+export default LavoriProgrammatiComponent;
